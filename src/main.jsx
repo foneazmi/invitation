ca86397d9ee983d1a3efb3d6650784e00f86d894
Farkhan Azmi <ghost@Farkhans-MacBook-Pro.local>
2023-07-31 14:17:35 +0700

    test

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-const App = React.lazy(() => import("./App"));
+const App = React.lazy(() => import("./App.jsx"));
 import { FaRegPaperPlane } from "react-icons/fa";
 
 const Spinner = (
